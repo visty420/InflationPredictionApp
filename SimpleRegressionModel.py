b829d757a1bf0328574c6c28704167fe42acf031
visty <117592958+visty420@users.noreply.github.com>
2024-01-04 16:23:57 +0200

    Update SimpleRegressionModel.py

--- SimpleRegressionModel.py
+++ SimpleRegressionModel.py
@@ -1,22 +1,22 @@
 import pandas as pd
 import torch
 import torch.nn as nn
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
+
 import matplotlib.pyplot as plt
+
 data = pd.read_csv("C:/Users/manea/Desktop/Licenta/InflationPredictionApp/cpi_data.csv")
 
 selected_columns = ['DATE', 'CPILFESL']
 data = data[selected_columns]
 
-# Convertirea coloanei 'DATE' la tipul de dată datetime
 data['DATE'] = pd.to_datetime(data['DATE'])
-
-# Setează coloana 'DATE' ca index
 data.set_index('DATE', inplace=True)
 
-# Normalizarea datelor
+# Standardizarea datelor
 scaler = StandardScaler()
 data['CPILFESL'] = scaler.fit_transform(data['CPILFESL'].values.reshape(-1, 1))
 
@@ -24,7 +24,6 @@ data['CPILFESL'] = scaler.fit_transform(data['CPILFESL'].values.reshape(-1, 1))
 train_size = int(len(data) * 0.80)
 train_data, test_data = data.iloc[:train_size], data.iloc[train_size:]
 
-# Funcția pentru pregătirea datelor în loturi
 def create_sequences(data, seq_length):
     sequences = []
     targets = []
@@ -35,11 +34,8 @@ def create_sequences(data, seq_length):
         sequences.append(seq.values)
         targets.append(target.values)
 
-    return torch.tensor(sequences), torch.tensor(targets)
+    return torch.tensor(sequences, dtype=torch.float32), torch.tensor(targets, dtype=torch.float32)
 
-# Creează secvențe pentru setul de antrenare
 seq_length = 10
 X_train, y_train = create_sequences(train_data, seq_length)
-
-# Creează secvențe pentru setul de testare
 X_test, y_test = create_sequences(test_data, seq_length)
